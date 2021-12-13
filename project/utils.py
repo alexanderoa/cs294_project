177ab20d55e51084ccff776e65df8053f1cf75d0
Alexander Asemota <alex@Alexanders-MacBook-Pro.local>
2021-12-13 08:09:56 -0800

    added funtions for pictures

--- project/utils.py
+++ project/utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #straight from strategic_rep repo
 def from_numpy_to_panda_df(data):
@@ -23,3 +25,47 @@ def create_dataset(data_size, covariance=None, d=1):
     labels = list(data.sum(axis=1).apply(map_sum_one_minus_one))
     data.insert(len(data.columns), 'target', labels, allow_duplicates=True)
     return data
+
+def plot_sim(plot_df, model):
+    sns.set_style('whitegrid')
+    sns.set_context('talk')
+    ax = plt.figure(figsize = (10,10))
+    ax = sns.scatterplot(data=plot_df, x='f0', y='f1', hue='target', palette=sns.color_palette('tab10')[:2])
+    for i in range(len(plot_df)):
+        if plot_df['f0_after'].loc[i] - plot_df['f0'].loc[i] < 0.05:
+            continue
+        ax.arrow(x=plot_df['f0'].loc[i],
+                 y=plot_df['f1'].loc[i],
+                 dx=plot_df['f0_after'].loc[i] - plot_df['f0'].loc[i],
+                 dy=plot_df['f1_after'].loc[i] - plot_df['f1'].loc[i],
+                 width=0.005,
+                 head_width=0.05,
+                 head_length=0.05,
+                 color='red',
+                 alpha=0.5)
+    t = np.arange(-2, 4, 0.2)
+    plt.plot(t, -t - model.intercept_, zorder=0, label='Strategic')
+    plt.plot(t, -t - model.intercept_ - 2, '--', zorder=0, label='Non-strategic')
+    plt.xlim(-2, 2.5)
+    plt.ylim(-2, 2.5)
+
+def plot_sim_iter(plot_df, model, i, axes):
+    sns.scatterplot(data=plot_df, x='f0', y='f1', hue='target',
+                              palette=sns.color_palette('tab10')[:2], ax = axes[i])
+    for i in range(len(plot_df)):
+        if plot_df['f0_after'].loc[i] - plot_df['f0'].loc[i] < 0.05:
+            continue
+        axes[i].arrow(x=plot_df['f0'].loc[i],
+                 y=plot_df['f1'].loc[i],
+                 dx=plot_df['f0_after'].loc[i] - plot_df['f0'].loc[i],
+                 dy=plot_df['f1_after'].loc[i] - plot_df['f1'].loc[i],
+                 width=0.005,
+                 head_width=0.05,
+                 head_length=0.05,
+                 color='red',
+                 alpha=0.5)
+    t = np.arange(-2, 4, 0.2)
+    axes[i].plot(t, -t - model.intercept_, zorder=0)
+    axes[i].plot(t, -t - model.intercept_ - 2, '--', zorder=0)
+    axes[i].xlim(-2, 2)
+    axes[i].ylim(-2, 2)
